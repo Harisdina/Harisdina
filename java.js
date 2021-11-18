00c785ef13167780e4135d20c5092b894e41b740
DHINESH <dhinesh@harisdina.harisdina>
2021-11-18 17:52:22 +0530

    new

---
+++ java.js
@@ -0,0 +1,15 @@
+if (5 === 10) {
+
+console.log('Hello World!'); // Skips this code
+
+} else if (10 === 10) {
+
+console.log('Hello World!'); // Prints Hello World! to the console
+
+
+
+} else {
+
+console.log('ERROR ERROR ERROR');
+
+};
